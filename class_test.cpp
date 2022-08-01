330a0a6f0ebe8aa49a0bc144aa1836ae785a1cd0
chankonixi <chengwy5@mail2.sysu.edu.cn>
2022-08-01 19:27:38 +0800

    增加了点类的头文件

--- class_test.cpp
+++ class_test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 const double PI = 3.14;
@@ -13,13 +14,28 @@ public:
     {
         cout<<radius<<endl;
     }
+    void setnumber()
+    {
+        radius = 1;
+        Id = "2";
+        height = 7;
+    }
 private:
     int radius;
+public:
+    string Id;
+protected:
+    int height;
 };
 
 int main()
 {
     circle c1;
     c1.r2radius(3);
+    c1.setnumber();
     c1.printradius();
+    c1.Id="77";
+    cout<<c1.Id<<endl;
+    circle *p = &c1;
+    p->printradius();
 }
