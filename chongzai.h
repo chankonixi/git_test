15f7e6d6e253befb8ef8e401a3d7eb17fd8af294
chankonixi <chengwy5@mail2.sysu.edu.cn>
2022-08-02 14:06:29 +0800

    学习函数重载

---
+++ chongzai.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+#ifndef CHONGZAI_H_
+#define CHONGZAI_H_
+
+void chongz();
+void chongz(int a);
+int chongz(int a, int b);
+
+#endif
--- class_test.cpp
+++ class_test.cpp
@@ -2,16 +2,43 @@
 #include <string>
 #include "point.h"
 #include "circle.h"
+#include "chongzai.h"
 using namespace std;
+
+void chongz()
+{
+    cout<< "无参数调用" <<endl;
+}
+
+void chongz(int a)
+{
+    cout<< "一个参数调用" <<endl;
+}
+
+int chongz(int a, int b)
+{
+    cout<< "两个参数调用" <<endl;
+    return 0;
+}
+
+void diaoyong()
+{
+    int a=10;
+    chongz();
+    chongz(10);
+    chongz(a);
+    chongz(1,2);
+}
 
 int main()
 {
-    //鎺ㄩ�乬ithub
-    circle c1;
-    point p1;
-    p1.setlocation(2,4);
-    c1.setlocation(5,6);
-    c1.r2radius(1);
-    c1.determine(p1);
+
+    // circle c1;
+    // point p1;
+    // p1.setlocation(2,4);
+    // c1.setlocation(5,6);
+    // c1.r2radius(1);
+    // c1.determine(p1);
+    diaoyong();
     return 0;
 }
